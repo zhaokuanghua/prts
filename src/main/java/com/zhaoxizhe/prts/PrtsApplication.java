d871c140cb073d1ef0c2419413825e3fafef943f
zhaokuanghua <937014825@qq.com>
2020-08-11 10:47:30 +0800

    prts

---
+++ src/main/java/com/zhaoxizhe/prts/PrtsApplication.java
@@ -0,0 +1,13 @@
+package com.zhaoxizhe.prts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhaoxizhe/prts/PrtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaoxizhe.prts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
